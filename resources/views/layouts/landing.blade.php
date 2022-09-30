229137874c178ae54f33a15fd786f9c8cb0ba5c5
noni227 <81325180+Noni2207@users.noreply.github.com>
2022-09-30 18:59:36 +0200

    created dynamic menu

--- resources/views/layouts/landing.blade.php
+++ resources/views/layouts/landing.blade.php
@@ -25,46 +25,7 @@
     </head>
     
     <section id="welcome">
-    <div class="welcome-menu">
-      <div class="logo">
-        <img src="https://webstockreview.net/images/clipart-restaurant-restaurant-logo-5.png">
-      </div>
-      <div class="menu">
-        <div class="menu-title">
-          Menu
-        </div>
-        <ul class="links">
-          <li>
-            <a href="#">Menu</a>
-          </li>
-          <li>
-            <a href="#">Waitlist</a>
-          </li>
-          <li>
-            <a href="#">Offers</a>
-          </li>
-          <li>
-            <a href="#">Giftcards</a>
-          </li>
-        </ul>
-      </div>
-      <div class="social-icons">
-        <a href="#">
-          <i class="fa fa-facebook" aria-hidden="true"></i>
-        </a>
-        <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
-        <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
-      </div>
-      <div class="location">
-        <div class="address">
-          234 Main st,<br>
-          New York, New York 11747
-        </div>
-        <div class="phone-number">
-          <a href="tel:7182198652">718-219-8652</a>
-        </div>
-      </div>
-    </div>
+    @include('includes.side-menu')
     <div class="welcome-jumbo">
       <div class="status">NEW</div>
       <h1>Billy Burger</h1>
