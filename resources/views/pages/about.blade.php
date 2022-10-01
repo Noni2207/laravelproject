0ddb40e7d33d85c1de14f1baf741dd72bf8fa7b3
noni227 <81325180+Noni2207@users.noreply.github.com>
2022-10-01 12:55:34 +0200

    update about page

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,7 +1,32 @@
 @extends('layouts.app')
  
 @section('content')
-<div class="about-page">
-     <h1> About Page</h1>
+<div id="about-page">
+    <div class="content-box">
+        <div class="row">
+            <div class="col-md-6">
+               <h1> About Page</h1>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+                     nisi ut aliquip ex ea commodo consequat.
+                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum 
+                     dolore eu fugiat nulla pariatur. 
+                     Excepteur sint occaecat cupidatat non proident,
+                    sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+                     nisi ut aliquip ex ea commodo consequat.
+                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum 
+                     dolore eu fugiat nulla pariatur. 
+                     Excepteur sint occaecat cupidatat non proident,
+                    sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+            </div>
+            <div class="col-md-6">
+                <img src="img/about-page.jpg" class="img-fluid" alt="...">
+            </div>
+        </div>
     </div>
+</div>
 @endsection
