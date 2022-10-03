30a0746051c798a69033d671f5a6a2f5f52c73a2
noni227 <81325180+Noni2207@users.noreply.github.com>
2022-10-03 14:51:57 +0200

    Waitlist Page HTML and CSS

--- resources/views/pages/waitlist.blade.php
+++ resources/views/pages/waitlist.blade.php
@@ -1,7 +1,53 @@
 @extends('layouts.app')
  
 @section('content')
-<div class="">
-     <h1> Weitlist Page</h1>
+<div id="waitlist-page">
+    <div class="content-box">
+        <div class="row">
+            <div class="col-md-6">
+               <h1> Get On The List</h1>
+<form>
+  <div class="form-group">
+    <label for="emailinput">Email address</label>
+    <input type="email" class="form-control" name="email" id="emailinput" placeholder="name@example.com">
+  </div>
+  <div class="form-group">
+    <label for="phoneinput">Phone #</label>
+    <input type="phone" class="form-control" name="email" id="phoneinput" placeholder="781-298-5768">
+  </div>
+  <div class="form-group">
+    <label for="guestsinput">How Many Guest</label>
+    <select class="form-control" name="guest" id="guestsinput">
+      <option value="6">1</option>
+      <option value="2">2</option>
+      <option value="3">3</option>
+      <option value="4">4</option>
+      <option value="5">5</option>
+    </select>
+  </div>
+  <div class="form-group">
+    <label for="timeinput">What Time?</label>
+    <select class="form-control" name="time" id="timeinput">
+      <option value="6">6:00 PM</option>
+      <option value="7">7:00 PM</option>
+      <option value="8">8:00 PM</option>
+      <option value="9">9:00 PM</option>
+      <option value="10">10:00 PM</option>
+    </select>
+  </div>
+  <div class="form-group">
+    <button class="btn btn-primary mb-2" type="submit">Comfirm</button>
+  </div>
+  </form>
+        </div>
+        <div class="col-md-6">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+                 nisi ut aliquip ex ea commodo consequat.
+                 </p>
+        </div>
     </div>
+</div>
+</div>
 @endsection
