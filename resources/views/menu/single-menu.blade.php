55f8419c5c71c3d28961232587aa07fc9be8c85c
noni227 <81325180+Noni2207@users.noreply.github.com>
2022-10-06 15:11:09 +0200

    single menu page

--- resources/views/menu/single-menu.blade.php
+++ resources/views/menu/single-menu.blade.php
@@ -2,7 +2,53 @@
  
 @section('content')
    <div id="single-menu-page">
-test
-
+      <div class="row">
+         <div class="col-sm-12 col-md-10 offset-md-1 col-lg-8 offset-lg-2">
+            <div class="content-box">
+               <div class="row">
+                  <div class="col-md-12">
+                     <h1>Burgers</h1>
+                  </div>
+                 <div class="col-md-6 item">
+                    <div class="title">
+                    <h4>Texas Burger</h4>
+                    <span class="price">$9</span>
+                    </div>
+                    <div class="description">
+                       <p>Bread, Meat, Tomato, Cheese, BBQ Sause </p>
+                    </div>
+                  </div>
+                 <div class="col-md-6 item">
+                    <div class="title">
+                    <h4>Texas Burger</h4>
+                    <span class="price">$9</span>
+                    </div>
+                    <div class="description">
+                       <p>Bread, Meat, Tomato, Cheese, BBQ Sause </p>
+                    </div>
+                  </div>
+                 <div class="col-md-6 item">
+                    <div class="title">
+                    <h4>Texas Burger</h4>
+                    <span class="price">$9</span>
+                    </div>
+                    <div class="description">
+                       <p>Bread, Meat, Tomato, Cheese, BBQ Sause </p>
+                    </div>
+                  </div>
+                 <div class="col-md-6 item">
+                    <div class="title">
+                    <h4>Texas Burger</h4>
+                    <span class="price">$9</span>
+                    </div>
+                    <div class="description">
+                       <p>Bread, Meat, Tomato, Cheese, BBQ Sause </p>
+                    </div>
+                  </div>
+                 </div>
+              </div>
+             </div>
+         </div>
+      </div>
    </div>
 @endsection
