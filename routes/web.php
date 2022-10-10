6cb5ea95b85f88f028805b42cd2aeb13fa24580f
noni227 <81325180+Noni2207@users.noreply.github.com>
2022-10-10 18:36:37 +0200

    Making (edit form)-page for food categories

--- routes/web.php
+++ routes/web.php
@@ -28,6 +28,10 @@ Route::get('/admin/food-categories/create', function () {
     return view('admin/food-categories/create');
 });
 
+Route::get('/admin/food-categories/{id}/edit', function () {
+    return view('admin/food-categories/edit');
+});
+
 Route::get('/admin/register', function () {
     return view('admin/register');
 });
