4d1f6c6891bfcef675fd80bc5e071ed59bceb2ef
noni227 <81325180+Noni2207@users.noreply.github.com>
2022-10-18 12:41:54 +0200

    Fixing for authentication complete

--- app/Http/Controllers/admin/AdminController.php
+++ app/Http/Controllers/admin/AdminController.php
@@ -6,8 +6,14 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
-{
+{  
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
     public function dashboard(){
         return view('admin/dashboard');
     }
+    
 }
--- app/Http/Controllers/admin/CustomersController.php
+++ app/Http/Controllers/admin/CustomersController.php
@@ -6,10 +6,16 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class CustomersController extends Controller
-{
+{   
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function allOffersMembers(){
         return view('admin/customers/all-offers-members');
     }
+    
     public function allReservation(){
         return view('admin/customers/all-reservation');
     }
