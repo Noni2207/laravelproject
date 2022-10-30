b499e895c1f9b31a0bbf0d408990b0491f0f7b98
noni227 <81325180+Noni2207@users.noreply.github.com>
2022-10-30 10:06:42 +0100

    Creating Seeds for Food Items

--- database/seeders/FoodCategorySeeder.php
+++ database/seeders/FoodCategorySeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Carbon\Carbon;
 
 class FoodCategorySeeder extends Seeder
 {
--- database/seeders/FoodItemSeeder.php
+++ database/seeders/FoodItemSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Carbon\Carbon;
 
 class FoodItemSeeder extends Seeder
 {
@@ -14,6 +15,41 @@ class FoodItemSeeder extends Seeder
      */
     public function run()
     {
-        //
+        \DB::table('food_items')->insert([
+            'title' => 'Texas Burger',
+            'decsription' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit, reiciendis aliquam vel voluptas repellendus natus nihil? ',
+            'image_url' => '/img/CHIPS.png',
+            'price' => 9.99,
+            'category_id' => 2,
+            'update_at' => Carbon::now(),
+            'created_at' => Carbon::now()
+        ]);
+        \DB::table('food_items')->insert([
+            'title' => 'BBQ Burger',
+            'decsription' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit, reiciendis aliquam vel voluptas repellendus natus nihil? ',
+            'image_url' => '/img/CHIPS.png',
+            'price' => 9.99,
+            'category_id' => 2,
+            'update_at' => Carbon::now(),
+            'created_at' => Carbon::now()
+        ]);
+        \DB::table('food_items')->insert([
+            'title' => 'Big Burger',
+            'decsription' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit, reiciendis aliquam vel voluptas repellendus natus nihil? ',
+            'image_url' => '/img/CHIPS.png',
+            'price' => 9.99,
+            'category_id' => 2,
+            'update_at' => Carbon::now(),
+            'created_at' => Carbon::now()
+        ]);
+        \DB::table('food_items')->insert([
+            'title' => 'Billvs Burger',
+            'decsription' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit, reiciendis aliquam vel voluptas repellendus natus nihil? ',
+            'image_url' => '/img/CHIPS.png',
+            'price' => 9.99,
+            'category_id' => 2,
+            'update_at' => Carbon::now(),
+            'created_at' => Carbon::now()
+        ]);
     }
 }
